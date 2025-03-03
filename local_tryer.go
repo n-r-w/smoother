a6e5e3d95a774641856de9d72169517559e08308
Roman Nikulenkov <nrw@yandex.ru>
2025-03-03 18:34:09 +0300

    local tryer with multiplier

--- local_tryer.go
+++ local_tryer.go
@@ -7,25 +7,52 @@ import (
 	"time"
 )
 
+// LocalTryerOption is a function that configures a LocalTryer.
+type LocalTryerOption func(*LocalTryer)
+
+// WithMultiplier sets the multiplier for the rate limit.
+// It allows to scale local traffic down by a factor of `multiplier`, compared to the
+// overall traffic that would be allowed by a redis rate limiter with the same number of instances.
+// Default is 1.
+func WithMultiplier(multiplier float64) LocalTryerOption {
+	return func(r *LocalTryer) {
+		r.multiplier = multiplier
+	}
+}
+
 // LocalTryer implements a simple rate limiter.
 // It allows n requests per interval and return false if the request is not allowed.
 type LocalTryer struct {
 	mu          sync.Mutex
 	lastAllowed time.Time
 	minInterval time.Duration
+
+	multiplier float64
 }
 
 var _ Tryer = (*LocalTryer)(nil)
 
 // NewLocalTryer creates a new LocalTryer instance.
-func NewLocalTryer(rps int) (*LocalTryer, error) {
+func NewLocalTryer(rps int, opts ...LocalTryerOption) (*LocalTryer, error) {
 	if rps <= 0 {
 		return nil, fmt.Errorf("NewLocalTryer: invalid rps %d", rps)
 	}
 
-	return &LocalTryer{
-		minInterval: time.Second / time.Duration(rps),
-	}, nil
+	r := &LocalTryer{
+		multiplier: 1,
+	}
+
+	for _, opt := range opts {
+		opt(r)
+	}
+
+	if r.multiplier <= 0 {
+		return nil, fmt.Errorf("NewLocalTryer: multiplier must be positive %f", r.multiplier)
+	}
+
+	r.minInterval = time.Second * time.Duration(r.multiplier) / time.Duration(rps)
+
+	return r, nil
 }
 
 // TryTake attempts to take n requests.
